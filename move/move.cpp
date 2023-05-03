03f89270257604845ce48d058b85d4ad8761c005
Denis Vergun <blu.eagle@mail.ru>
2023-05-03 18:24:21 +0500

    cd move; make

--- move/move.cpp
+++ move/move.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 #include <vector>
 #include <string>
- 
+int func(auto u){return std::move(u);}; 
 int main()
 {
     std::string str = "Salut";
@@ -23,8 +23,11 @@ int main()
  
     std::cout << "The contents of the vector are { " << std::quoted(v[0])
                                              << ", " << std::quoted(v[1]) << " }\n";
-std::vector<int> v1 = {2, 3, 3};
-v1 = std::move(v1); // the value of v is unspecified
-std::cout<<v1[0]<<std::endl;
+//std::vector<int> v1 = {2, 3, 3};
+//v1 = std::move(v1); // the value of v is unspecified
+//std::cout<<v1[0]<<std::endl;
+//    static_assert(!true, "bad forward call");
+    std::cout<<func(std::forward<int>(7))<<std::endl;
+
 
 }
