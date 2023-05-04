0c578f6dd3ae7ff493ce7cd6ea38e95cd1f417ee
Denis Vergun <blu.eagle@mail.ru>
2023-05-05 00:16:10 +0500

    move/person.cpp

--- move/person.cpp
+++ move/person.cpp
@@ -3,11 +3,15 @@
 #include <iostream>
 using namespace std;
 struct Person{
+	string _name;
 	public:
 	Person(const string& name):_name{name}{}
 	Person(Person& p):_name{p._name}{}
 	//template<typename T> Person(T&& nam){_name=nam;};
-	string _name;
+	//string _name;
+	friend std::ostream& operator << (std::ostream & strm, const Person& p){
+	return strm << p._name << endl;
+	}
 };
 int main(){
 
@@ -16,6 +20,6 @@ string name="Herb";
 Person p2(name);
 
 Person p3(p1);
-cout<< Person{p1}._name<<endl;
+cout<< Person{p1};//<<endl;
 return 0;
 }
