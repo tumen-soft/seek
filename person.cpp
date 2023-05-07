771ea093929a7227dd79b2007328e3a1172a6e0a
Denis Vergun <blu.eagle@mail.ru>
2023-05-08 01:34:27 +0500

    g

---
+++ person.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <type_traits>
+#include <iostream>
+using namespace std;
+struct Person{
+	string _name;
+	public:
+	explicit Person(const string& name):_name{name}{}
+	//Person(Person& p):_name{p._name}{}
+	//template<typename T> Person(T&& nam){_name=nam;};
+	//string _name;
+	friend std::ostream& operator << (std::ostream & strm, const Person& p){
+	return strm << p._name << endl;
+	}
+};
+int main(){
+
+Person p1("Bjarne");
+string name="Herb";
+Person p2(name);
+
+Person p3(p1);
+cout<< Person{p2};//<<endl;
+return 0;
+}
