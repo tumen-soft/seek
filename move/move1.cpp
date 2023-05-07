c068ea77b2fbd27b9fdaa0eb3311e75ba1276b5f
Denis Vergun <blu.eagle@mail.ru>
2023-05-07 20:24:17 +0500

    cd move;make

--- move/move1.cpp
+++ move/move1.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 #include <utility>
-int func(int u=3){int j(u);;auto f=u;return ((j));}; 
-
-int f(int* f){return f=6;}
+int func(int u=3){int j(u);;auto f=u;return (std::move(f));}; 
 
 int main()
 {
     int* i=new int(5);
-//    while(true)
     std::cout<<func()<<std::endl;
-    std::cout<<func(new int(std::forward<int&>(*(i))))<<std::endl;
-    std::cout<<func(*(i))<<std::endl;
-    //i=3;
-    //int* j(&i);
-    //int** k(&(j));
-    //**k=2;
-    //std::cout<<**k<<std::endl;	
-    //free((int*)i);
-    //free(j);
+    std::cout<<func((std::forward<int&>(*(i))))<<std::endl;
+    std::cout<<func(std::forward<int&>(*(i)))<<std::endl;
     delete i;
-    int c=int(5);
-    std::cout<<f(&c)<<std::endl;
-	return 0;
+    return 0;
 }
