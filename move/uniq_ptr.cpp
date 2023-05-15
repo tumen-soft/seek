ced458bb4932751dbf6b5e357d7647b2b9ddc0df
Denis Vergun <blu.eagle@mail.ru>
2023-05-15 11:49:17 +0500

    cd  move;make

--- move/uniq_ptr.cpp
+++ move/uniq_ptr.cpp
@@ -16,15 +16,25 @@ int main(){
 	std::unique_ptr<A>unPtr1 = std::make_unique<A>();
 	}
 	std::cout <<"---------------------"<<std::endl;
+	
 
-	//std::unique_ptr<int>unPtr2 = *unPtr1;
-	std::shared_ptr<A>unPtr1 = std::make_shared<A>();
-	std::cout << unPtr1.use_count() << std::endl;
+	weak_ptr<int>wePtr1;
 	{
-	std::shared_ptr<A>unPtr2 = unPtr1;
-        std::cout << unPtr1.use_count() << std::endl;
+        std::shared_ptr<int>shPtr1 = std::make_shared<int>(25);
+	wePtr1 = shPtr1;
+	std::cout<< "wePtr1" <<std::endl;
 	}
-        std::cout << unPtr1.use_count() << std::endl;
+
+	{
+	//std::unique_ptr<int>unPtr2 = *unPtr1;
+	std::shared_ptr<A>shPtr1 = std::make_shared<A>();
+	std::cout << shPtr1.use_count() << std::endl;
+	{
+	std::shared_ptr<A>shPtr2 = shPtr1;
+        std::cout << shPtr1.use_count() << std::endl;
+	}
+        std::cout << shPtr1.use_count() << std::endl;
+	}
 	//char* g;
 	//scanf(g);
 //std::cout << *unPtr2 << std::endl;
