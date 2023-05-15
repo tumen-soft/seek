dd7de86daba5a96a86a37f6d2c97e5d371b2278a
Denis Vergun <blu.eagle@mail.ru>
2023-05-15 06:55:09 +0500

    cd  move;make

--- move/overload.cpp
+++ move/overload.cpp
@@ -1,18 +1,23 @@
 #include <string>
 #include <iostream>
-void Fun(int& i){
+class Fun{
+	public:
+	Fun(){
 	std::cout<<"ref"<<std::endl;
-}
+	}
+};
+/*
 void Fun(const int& i){
 	std::cout<<"const ref"<<std::endl;
 }
 void Fun(int&& i){
 	std::cout<<"moveref"<<std::endl;
 }
-
+*/
 int main(){
 int i=3;
-Fun(std::move(i));
-	//std::cout<<Fun()<<std::endl;
+//Fun Fun;
+Fun x = Fun();
+//	std::cout<<x<<std::endl;
 return 0;
 }
